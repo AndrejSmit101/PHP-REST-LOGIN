05aac9c4a0f10f8e5be7831767f073c63c5bdbfa
AndrejSmit101 <75758171+AndrejSmit101@users.noreply.github.com>
2021-08-23 15:30:09 +0200

    Update index.php

--- properties/index.php
+++ properties/index.php
@@ -1,11 +1,11 @@
 <?php
 require_once('../includes/init.php');
 session_start();
-//Proverava da li je $_SESSION['key'] true, ako nije to znaci da se stranica pristupa manuelno tj. ne kroz login formu.
+//This checks if the $_SESSION['key'] true. If it isn't that means that the page is being accessed manually, and not from the login form.
 if(!$_SESSION['key']) {
   header("Location: ../../");
 }
-//Ovo mogu iskoristiti u header, kod authorization.
+//This is used in properties/index.php
 $token = $_SESSION['key'];
 
 $url = "https://fws-api-test-be.herokuapp.com/api/properties?orderby=title&order=asc&items-per-page=10";
@@ -14,9 +14,9 @@ $headers = array(
   "Content-Type: application/json",
   "Authorization: Bearer $token"
 );
-//GET REQUEST metoda
+//GET REQUEST method
 $check = $request->getRequest($url, $headers);
-//U slucaju da GET metoda vrati false, to znaci da access token nije dobar. Prvih 3 minuta nece izaci ovaj error.
+//In case that GET method returns false, that means that the access token isn't valid. In the first 3 minutes this error won't come up.
 if(!$check) {
     die("Invalid Token, can't retrieve properties!");
 }
@@ -32,7 +32,7 @@ if(!$check) {
   </head>
   <body>
     <?php
-      //Prikazuje prvi Title u array. Objasnio sam zasto nije odradjeno sve.
+      //This shows the first title in the $check array.
       echo $check['properties']['data']['0']['title'];
     ?>
   </body>
